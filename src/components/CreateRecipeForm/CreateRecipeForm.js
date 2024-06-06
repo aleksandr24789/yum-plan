9d1fade2561c9aedf8abed1a2bedd02a26cf0c20
Aleksandr <87819638+aleksandr24789@users.noreply.github.com>
2024-06-06 06:01:43 +0000

    Add recipe picture to form values state

--- src/components/CreateRecipeForm/CreateRecipeForm.js
+++ src/components/CreateRecipeForm/CreateRecipeForm.js
@@ -93,7 +93,7 @@ export default function CreateRecipeForm() {
       >
         Изображение
       </label>
-      <RecipeImage />
+      <RecipeImage onChange={setFormValues} />
       <label
         htmlFor="duration"
       >
--- src/components/RecipeImage/RecipeImage.js
+++ src/components/RecipeImage/RecipeImage.js
@@ -1,16 +1,21 @@
 import { useState } from 'react'
 import NextImage from 'next/image'
 import defaultPicture from 'public/images/default-recipe-picture.jpg'
-import { toBase64 } from '@/utils/toBase64'
-import { resizeImage } from '@/utils/resizeImage'
+import { toBase64, resizeImage } from '@/helpers/images'
 import styles from './RecipeImage.module.css'
 
-export default function RecipeImage() {
+export default function RecipeImage(props) {
   const [picture, setPicture] = useState()
 
   const onChangePicture = async (e) => {
     const file = await toBase64(e.target.files[0])
-    setPicture(resizeImage(file, 130, 130))
+    const newPicture = await resizeImage(file, 130, 130)
+    setPicture(newPicture)
+    console.log('new picture', picture)
+    props.onChange(prev => ({
+      ...prev,
+      picture
+    }))
   }
 
   return (
